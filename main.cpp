b6779da0dd72fee87440f2f995c588208c18ad2b
Fidencio162 <fisicafide27@gmail.com>
2022-05-11 17:57:58 -0500

    cambiar_argumrnto

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <gsl/gsl_sf_bessel.h>
 
 int main( ) {
-    double x=5.0;
+    double x=8.0;
     double y= gsl_sf_bessel_J0(x);
     printf("J0(%g)=%.18e\n",x,y);
     std::cout << "Hello, World!" << std::endl;
